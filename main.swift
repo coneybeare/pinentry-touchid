90f173c61f02a23ab7edc50b06b91936612810f7
Matt Coneybeare <35369+coneybeare@users.noreply.github.com>
2023-04-23 11:17:15 -0400

    update commands

--- main.swift
+++ main.swift
@@ -1,5 +1,6 @@
 // pinentry-touchid - A barebones pinentry for macOS using Touch ID and Keychain.
 // Author: Matt Coneybeare <matt@coneybeare.me>
+// SPDX-License-Identifier: ISC
 
 import Foundation
 import LocalAuthentication
@@ -7,7 +8,7 @@ import Darwin.C
 
 let keychainServiceName = "me.coneybeare.matt.pinentry-touchid.password"
 let policy = LAPolicy.deviceOwnerAuthenticationWithBiometrics
-let reason = "sign your commit with gpg."
+let reason = "sign your commit with gpg"
 
 // Save the passphrase into Keychain.
 func setPassword(password: String) -> Bool  {
@@ -46,16 +47,15 @@ func interact() {
 
     var error: NSError?
     guard context.canEvaluatePolicy(policy, error: &error) else {
-        print("Your Mac doesn't support deviceOwnerAuthenticationWithBiometrics")
+        print("ERR 83886179 Your Mac doesn't support deviceOwnerAuthenticationWithBiometrics")
         exit(EXIT_FAILURE)
     }
 
-    print("OK. hello")
+    print("OK hello")
     while let input = readLine() {
         if input.lowercased().hasPrefix("setpass") {
-            // TODO: use a CLI option
             guard setPassword(password: "SUPERSECURE") else {
-                print("Failed to write the default password.")
+                print("ERR 83886179 Failed to write the default password.")
                 continue
             }
             print("OK. Password set to default value 'SUPERSECURE'. Change it using Keychain Access ASAP")
@@ -67,24 +67,23 @@ func interact() {
                 context.evaluatePolicy(policy, localizedReason: reason) { success, error in
                     if success && error == nil {
                         guard let password = getPassword() else {
-                            print("Failed to get password from keychain.")
+                            print("ERR 83886179 Failed to get password from keychain.")
                             return
                         }
                         print("D \(password)")
                         print("OK thanks.")
                     } else {
                         let errorDescription = error?.localizedDescription ?? "Unknown error"
-                        print("Error: \(errorDescription)")
+                        print("ERR 83886179: \(errorDescription)")
                     }
                 }
             case "bye":
                 print("OK bye.")
                 exit(EXIT_SUCCESS)
             default:
-                print("OK, command not recognized.")
+                print("OK command not recognized '\(input)'.")
         }
     }
-
     // on EOF, continue running until ^C
     dispatchMain()
 }
